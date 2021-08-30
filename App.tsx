9984bdd1611a3aefe4541456f9bdf6684d2331ec
Lucas Abdouni <67294018+lucasabdouni@users.noreply.github.com>
2021-08-30 18:33:59 -0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import 'react-native-gesture-handler';
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import AppLoading from 'expo-app-loading';
+import { ThemeProvider } from 'styled-components';
+
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from '@expo-google-fonts/poppins';
+
+import theme from './src/global/styles/theme';
+
+import { StatusBar } from 'react-native';
+
+import { AuthProvider, useAuth } from './src/hooks/auth';
+
+import { Routes } from './src/routes';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  const { userStorageLoading } = useAuth();
+
+  if (!fontsLoaded || userStorageLoading) {
+    return <AppLoading />;
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar barStyle="light-content" />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
+    </ThemeProvider>
+  );
+}
