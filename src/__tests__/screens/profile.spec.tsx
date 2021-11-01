caf4258dbdb4809551180b4ac01c67e33e9dc1b6
Lucas <lucasabdouni@hotmail.com>
2021-11-01 20:24:54 -0300

    starting tests

---
+++ src/__tests__/screens/profile.spec.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+
+import { Profile } from '../../screens/Profile';
+
+describe('Profile ', () => {
+  it('should have placeholder correctly in  ser name input', () => {
+    const { getByPlaceholderText } = render(<Profile />);
+
+    const inputName = getByPlaceholderText('Nome');
+
+    expect(inputName).toBeTruthy();
+  });
+
+  it('should be load user data', () => {
+    const { getByTestId } = render(<Profile />);
+
+    const inputName = getByTestId('input-name');
+    const inputSurname = getByTestId('input-surname');
+
+    expect(inputName.props.value).toEqual('Lucas');
+    expect(inputSurname.props.value).toEqual('Abdouni');
+  });
+
+  it('should exist title correctly', () => {
+    const { getByTestId } = render(<Profile />);
+
+    const textTitle = getByTestId('text-title');
+
+    expect(textTitle.props.children).toContain('Perfil');
+  });
+});
